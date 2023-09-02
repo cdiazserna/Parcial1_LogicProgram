9901e06c8e85dd1d33f665acc383cb514d28dd90
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 21:21:30 -0500

    Declaracion de variables

---
+++ Punto1a/Punto1a/Program.cs
@@ -0,0 +1,18 @@
+﻿
+public class Punto1
+{
+    static void Main(string[] args)
+    {
+        double X, A, B, C;
+
+        Random random = new Random();
+        A = random.Next(1, 9);
+        B = random.Next(1, 9);
+        C = random.Next(1, 9);
+
+        Console.Write("El valor de A es: " + A);
+        Console.Write("El valor de B es: " + B);
+        Console.Write("El valor de C es: " + C);
+       
+    }
+}
