9e0c05d848160085f6c74bdf5cf8e2d33c91ea77
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 21:53:33 -0500

    Declaracion de variables y ecuacion.

--- Punto2/Punto2/Program.cs
+++ Punto2/Punto2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Punto2
+{
+    static void Main(string[] args)
+    {
+        double horasTrabajadas, valorPorHora, salarioNeto, salarioFinal;
+        string nombreEmpleado, cargoEmpleado;
+
+        Console.Write("Ingrese el nombre del empleado: ");
+        nombreEmpleado = Console.ReadLine();
+        Console.Write("Ingrese el cargo del empleado: ");
+        String cargo = cargoEmpleado = Console.ReadLine();
+        Console.Write("Ingrese las horas trabajadas en el mes:");
+        horasTrabajadas = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor por hora trabajada: ");
+        valorPorHora = Convert.ToDouble(Console.ReadLine());
+
+        salarioNeto = horasTrabajadas * valorPorHora;
+
+        salarioFinal = (salarioNeto - ((((4 * salarioNeto) / 100) + (4 * salarioNeto) / 100) + (salarioNeto/4)));
+
+        Console.Write()
+    }
+
+
+
+
+}
