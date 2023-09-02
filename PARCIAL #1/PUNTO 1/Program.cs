61c9846d364fc16166881ec8928316cc7c508164
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2023-09-01 21:42:32 -0500

    empiezo declarando las variables que voy a necesitar para calcular la formula

--- PARCIAL #1/PUNTO 1/Program.cs
+++ PARCIAL #1/PUNTO 1/Program.cs
@@ -3,6 +3,12 @@ using System;
 
 class Program
 {
-    static void Main(string[args])
-    {
+    static void Main(string[] args);
+    double a, b, c, randomnumber;
+
+    Random randon =new Random();
+    randomnumber =Random.next(min,max)
+}
+   
+
        
