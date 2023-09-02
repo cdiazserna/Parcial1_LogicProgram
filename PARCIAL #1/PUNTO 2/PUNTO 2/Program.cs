a3354449e32ac1baadc1f3f9ab349731935c4564
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2023-09-01 20:39:15 -0500

    al mismo tiempo estoy declarando el tipo de variables para asi almacenarlas correctamente

---
+++ PARCIAL #1/PUNTO 2/PUNTO 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Por favor, ingrese nombre del empleado");
+        string nombre = (Console.ReadLine());
+
+        Console.WriteLine("Por favor, ingrese el cargo del empleado");
+        string cargo = (Console.ReadLine());
+
+        Console.WriteLine("Por favor, ingrese la cantidad de horas trabajadas");
+        double horas = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Por favor, ingrese la cantidad de horas trabajadas");
+        double vhora = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Los números comprendidos entre " + numero1 + " y " + numero2 + " son:");
+
+       
+}
+
