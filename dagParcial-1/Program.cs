7a68b4fa957b2e3a07c96788269377618a5d98fa
Daniel Amado Garcia <danielamadogarcia8@gmail.com>
2023-09-01 20:54:31 -0500

    Asignamos variables

---
+++ dagParcial-1/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+namespace Punto1
+{
+    public class Punto1
+    {
+        static void Main(string[] args)
+        {
+            //Realice un programa en C# donde se tenga la posibilidad de evaluar la siguiente expresión matemática  
+            // y me permita encontrar el valor de la variable "x":
+            double a, b, c, x;
+
+            Random random = new Random();
+
+            a = random.Next(2, 10);
+
+            b = random.Next(2, 10);
+
+            c = random.Next(2, 10);
+
+            
+
+
+   
+        
+     
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
