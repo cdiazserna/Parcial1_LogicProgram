c5ea434d3493f57135bb208d56b28a35a48620ae
santi04 <santivelas5@gmail.com>
2023-09-01 21:16:08 -0500

    avance 2

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -8,24 +8,34 @@
         //instancia clase ramdon o datos de entrada
         Random random = new Random();
 
-        a = random.Next(0, 8);
-        b = random.Next(0, 8);
-        c = random.Next(0, 8);
+        a = random.Next(1, 8);
+        b = random.Next(1, 8);
+        c = random.Next(1, 8);
 
 
         //proceso
 
         solucionNumerador = (3 * a + 4 * b);
+        Console.WriteLine(solucionNumerador);
         solucionNumerador = Math.Pow(solucionNumerador, 3);
+        Console.WriteLine(solucionNumerador);
         solucionNumerador = Math.Pow(solucionNumerador, 2);
+        Console.WriteLine(solucionNumerador);
         solucionNumerador = solucionNumerador * c;
+        Console.WriteLine(solucionNumerador);
 
         solucionDenominador = (2 * c) + a;
+        Console.WriteLine(solucionDenominador);
         solucionDenominador = Math.Pow(solucionDenominador, 2);
+        Console.WriteLine(solucionDenominador);
 
-        x = Math.Sqrt(solucionNumerador/solucionDenominador);
+
+        x = solucionNumerador / solucionDenominador;
+        x = Math.Sqrt(x);
 
         //salida
+
+        Console.WriteLine(x);
     }
 
 
