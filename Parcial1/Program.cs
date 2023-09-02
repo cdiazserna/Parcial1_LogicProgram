5a39cd328df86130f162e9545a3a40bc5132e690
Xkals <141454035+Xkals@users.noreply.github.com>
2023-09-01 20:58:04 -0500

    Envio 3

--- Parcial1/Program.cs
+++ Parcial1/Program.cs
@@ -18,10 +18,11 @@ namespace Parcial1
             a = randomNumber1;
             b = randomNumber2;
             c = randomNumber3;
-
+            // Operacion
             operacion1 = Math.Pow(Math.Pow((3 * a + 4 * b), 3),2) * c ;
             operacion2 = Math.Pow((2 * c + a), 2);
             operacion3 = Math.Sqrt (operacion1 / operacion2);
+            // Pantalla
 
                 Console.WriteLine($"El valor de  x cuando a es ({ a}),b es ({b}) y c es ({c}) X = {operacion3} ");
                 
