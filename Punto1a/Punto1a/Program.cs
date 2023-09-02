34772b833cc015089d4bfd2300253147a561c7de
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 21:28:17 -0500

    Poryecto Finalizado, ES ESTE.

--- Punto1a/Punto1a/Program.cs
+++ Punto1a/Punto1a/Program.cs
@@ -13,6 +13,8 @@ public class Punto1
         Console.Write("El valor de A es: " + A);
         Console.Write("El valor de B es: " + B);
         Console.Write("El valor de C es: " + C);
-       
+
+        X = Math.Sqrt((Math.Pow(Math.Pow(3 * A + 4 * B, 3), 2) * C) / Math.Pow(2 * C + A, 2));
+        Console.Write("El valor de X es: " +  X);
     }
 }
