2894f657ba77f042d42ef699347ab0b3f19bc6b6
sebasredes19 <sebastianespinosavalencia@hotmail.com>
2023-09-01 21:40:50 -0500

    Calcular Salario Neto

--- Punto2/Punto2/Program.cs
+++ Punto2/Punto2/Program.cs
@@ -14,6 +14,37 @@
         Console.WriteLine("Ingrese el nombre del empleado: ");
         string nombre = Console.ReadLine();
 
-       
+        Console.WriteLine("Ingrese el cargo del empleado: ");
+        string cargo = Console.ReadLine();
+
+        Console.WriteLine("Ingrese las horas trabajadas: ");
+        int horasTrabajadas = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de la hora: ");
+        double valorHora = double.Parse(Console.ReadLine());
+
+        double salarioNeto = CalcularSalarioNeto(horasTrabajadas, valorHora);
+        double salarioFinal = CalcularSalarioFinal(salarioNeto);
+
+        Console.WriteLine("Nombre del empleado: " + nombre);
+        Console.WriteLine("Cargo: " + cargo);
+        Console.WriteLine("Salario Neto (antes de deducciones): $" + salarioNeto);
+        Console.WriteLine("Salario Final (después de deducciones): $" + salarioFinal);
+    }
+
+    static double CalcularSalarioNeto(int horasTrabajadas, double valorHora)
+    {
+        double salarioBruto = horasTrabajadas * valorHora;
+        return salarioBruto;
+    }
+
+    static double CalcularSalarioFinal(double salarioNeto)
+    {
+        double salud = salarioNeto * 0.04;
+        double pension = salarioNeto * 0.04;
+        double embargo = salarioNeto / 4;
+
+        double salarioFinal = salarioNeto - salud - pension - embargo;
+        return salarioFinal;
     }
 }
