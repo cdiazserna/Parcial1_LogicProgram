5a3f7a57e5e4121821079aca169f5f53e1957364
JUAN PABLO UPEGUI ORTIZ <upegui2007@hotmail.com>
2023-09-01 21:16:49 -0500

    CALCULAR VALORES

--- PUNTO1/Program.cs
+++ PUNTO1/Program.cs
@@ -7,7 +7,7 @@ public class PUNTO1
     static void MAIN(string[] args)
     {
         //DATOS DE ENTRADA
-        int A, B, C, Radom,Randomnumber, Min,Max, VALOR1, VALOR2, VALOR3, X;
+        int A, B, C, Radom,Randomnumber, Min,Max, VALOR1, VALOR2, VALOR3,VALOR4,VALOR5,VALOR6, X;
         Random random = new Random();
         Randomnumber = Radom.Next(Min, Max);
         Min < 8;
@@ -15,11 +15,30 @@ public class PUNTO1
              
 
         Console.WriteLine("INGRESA EL VALOR DE A");
-        A = int Randomnumber(Console.ReadLine())
-     ;
+        A =(Console.ReadLine());
+     
         Console.WriteLine("INGRESA EL VALOR DE B");
-        B = Randomnumber (Console.ReadLine())
+        B = Randomnumber(Console.ReadLine());
 
+        Console.WriteLine("INGRESA EL VALOR DE C");
+        C = Randomnumber(Console.ReadLine());
+
+
+        //Calculamos Valor1 valor2 y valor3
+
+        VALOR1 = ((3 * A) +( 4 * B));
+
+        VALOR2 = VALOR1 * VALOR1 * VALOR1;
+
+        VALOR3 = (2 * C) + A;
+
+        VALOR4 = VALOR3 * VALOR3;
+
+        VALOR5 = VALOR2 * VALOR2;
+
+        VALOR6 = VALOR5 * C;
+
+        X = Math.Sqrt(VALOR6 / VALOR4);
 
 
 
