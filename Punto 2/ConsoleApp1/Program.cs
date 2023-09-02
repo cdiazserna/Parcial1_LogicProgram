cbd999f5a79bce98ff445a73b05354c891d1f397
Marlon Foronda <forondamarlon642@gmail.com>
2023-09-01 21:37:47 -0500

    solución punto 2 parcial 1

---
+++ Punto 2/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿//1 definir el nombre de la clase con su respectivo metodo main
+public class MiPrimerPuntoTaller
+{
+    static void Main(string[] args)
+    {   //declaración de mis variables
+        int nomina, salario, salud, pensión, banco;
+        //pedir valores de consola 
+        Console.Write("Ingrese el nombre del empleado");
+        Console.Write("Ingrese el cargo del empleado");
+        console.write("Ingrese el salario neto del empleado");
+        console.write("Ingrese el nombre del empleado");
+
+        //4to Calculos (procesos)
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
