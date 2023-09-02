4bdaefe0f34f1f08f6561ccdfd34b62e8f0dee31
carlosmena2191 <carloselrussomena5@gmail.com>
2023-09-01 20:58:31 -0500

    Punto1

---
+++ Punto1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Punto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random =new Random();
+            int min = 1; // valor minimo permitido
+            int max = 0; // valor maximo permitido
+
+            // Generar valores aleatorios para a, b y c en el rango [min, max]
+            int a = random.Next(min, max);
+            int b = random.Next(min, max);
+            int c = random.Next(min, max);
+        }
+    }
+}
