8f8ac7e34bffa746663e0a416bfa93a06936777f
Julian1824 <julianbs1813@gmail.com>
2023-09-01 19:11:13 -0600

    Se crea el numero random

--- Punto 1/Punto 1/Program.cs
+++ Punto 1/Punto 1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Punto_1
+{
+
+    static void Main()
+    {
+        double x, a, b, c,randomNumber;
+
+        Random rand = new Random();
+
+        randomNumber = rand.Next(10,18);
+
+        Console.WriteLine(randomNumber);
+    }
+}
