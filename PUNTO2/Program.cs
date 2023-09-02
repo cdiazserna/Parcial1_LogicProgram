13b512adccf5fed203e42cf162daf3a4092bda9f
JUAN PABLO UPEGUI ORTIZ <upegui2007@hotmail.com>
2023-09-01 21:46:37 -0500

    Datos de entrada

--- PUNTO2/Program.cs
+++ PUNTO2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Punto 2: Desarrolle un algoritmo en C# que me permita calcular la nómina del mes de un empleado. 
+//Debemos pedir por pantalla el nombre del empleado, el cargo, horas trabajadas y valor de la hora.
+//Console.WriteLine("Hello, World!");
+
+public class PUNTO2
+{
+    static void Main(string[] arg)
+    {
+
+        //datos de entrada
+        int Empleado, Cargo, Horas, ValorHora, Salud, Pension, Embargo, SalarioNeto, SalarioTotal;
+
+        Console.WriteLine("Ingresa el Nombre del Empleado");
+        Empleado = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+    }
+
+
+
+}
