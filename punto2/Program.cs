8b780981fb01755fbe1991a003ada728961f549a
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-01 21:54:01 -0500

    salario final

--- punto2/Program.cs
+++ punto2/Program.cs
@@ -12,6 +12,28 @@ namespace punto1
             //declaracion de variables
             double nomEmple, cargoEmple, horasTrab, valorHora;
 
+            //solicitar datos de entrada por pantalla
+
+            Console.Write("Por favor ingresar el nombre del empleado: ");
+            nomEmple = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Por favor ingresar el cargo del empleado: ");
+            cargoEmple = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Por favor ingresar las horas trabajadas del empleado: ");
+            horasTrab = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Por favor ingresar las horas trabajadas del empleado: ");
+            valorHora  = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+
+
+
+
 
 
 
