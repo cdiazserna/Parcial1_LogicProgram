89f4afe87daaa122b8ebc1c0cb5a7510e2b725d2
SANTIAGOGALVIS04 <sg1904@gmail.com>
2023-09-01 20:34:26 -0500

    Realizamos la definición del tipo de variable que vamos a utilizar

---
+++ punto1/punto1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace punto1
+{
+    public class Punto1
+    {
+        static void Main(string[] args)
+        {
+            //Realice un programa en C# donde se tenga la posibilidad de evaluar una ecuación matemática y permita encontrar 
+            //el valor de la variable
+
+            //Defino primeramente las variables
+            int a, b, c, x;
+
+
+
+
+        }
+    }
+}
