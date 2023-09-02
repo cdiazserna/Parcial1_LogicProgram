925f3bbbbf4425048d9d9e9c89ef7ebfc8acf526
jenihernandez <jenihernandez.98@gmail.com>
2023-09-01 21:28:57 -0500

    terminado

--- PARCIAL1/PARCIAL1/Program.cs
+++ PARCIAL1/PARCIAL1/Program.cs
@@ -7,32 +7,39 @@ namespace PARCIAL1
         static void Main(string[] args)
         {
             //Pedir en pantalla
-            Console.WriteLine("Ingrese nombre del empleado: ");
+            Console.Write("Ingrese nombre del empleado: ");
             string nombre = Console.ReadLine();
 
-            Console.WriteLine("Cargo del empleado: ");
+            Console.Write("Cargo del empleado: ");
             string cargo = Console.ReadLine();
 
-            Console.WriteLine("Horas trabajadas por el empleado: ");
+            Console.Write("Horas trabajadas por el empleado: ");
             int horasTrabajadas = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Valor de la hora: ");
+            Console.Write("Valor de la hora: ");
             double valorHora = double.Parse(Console.ReadLine());
 
+          
             //Calcular deducciones
-            double salarioNeto = horasTrabajadas * valorHora;
+            double salarioNeto = (int)(horasTrabajadas * valorHora);
 
             double salud = salarioNeto * 0.04;
 
             double pension = salarioNeto * 0.04;
 
-            double embargo = salarioNeto / 0.04;
+            double embargo = salarioNeto / 4;
 
             double salarioTotal = salarioNeto - salud - pension - embargo;
 
-            //
+            //Mostrar en pantalla
+            Console.WriteLine("nombre del empleado: " + nombre);
 
+            Console.WriteLine("Cargo del empleado: " + cargo);
 
+            Console.WriteLine("Salario neto (sin deducciones): " + salarioNeto);
+
+            Console.WriteLine("Salario total: " + salarioTotal);
+            
         }
     }
 }
