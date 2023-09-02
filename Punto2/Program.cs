9620d7d77c9714e51b2ccbf0409c04d6914f7d1c
Sebastián Zerpa Vanegas <sebastianzerpav@outlook.com>
2023-09-01 21:30:56 -0500

    commit constantes

--- Punto2/Program.cs
+++ Punto2/Program.cs
@@ -7,4 +7,17 @@ public class NominaMensualEmpleado
         //Declaración de variables
         string nombreEmpleado, cargoEmpleado;
         int horasTrabajadas;
-        decimal valorHorasTrabajadas;
+        decimal valorHorasTrabajadas, nominaSinDeducciones, nominaConDeducciones;
+
+        //Declaración de constantes
+        const double porcentajeSalud = 4 / 100;
+        const double porcentajePension = 4 / 100;
+        const decimal embargo = 1 / 4;
+
+        //Pedir datos de entrada
+
+    }
+
+}
+
+
