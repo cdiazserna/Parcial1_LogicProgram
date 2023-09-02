66966fdfa19d9fb43d7da7fb7235baa9e394b85f
Daniel Amado Garcia <danielamadogarcia8@gmail.com>
2023-09-01 21:41:52 -0500

    Ejecución

--- Punto2/Program.cs
+++ Punto2/Program.cs
@@ -15,3 +15,24 @@ class Program
         Console.Write("Valor de la hora: ");
         double valorHora = Convert.ToDouble(Console.ReadLine());
 
+        // Calcular salario bruto
+        double salarioBruto = horasTrabajadas * valorHora;
+
+        // Calcular deducciones
+        double salud = salarioBruto * 0.04;
+        double pension = salarioBruto * 0.04;
+        double embargo = salarioBruto / 4;
+
+        // Calcular salario neto
+        double salarioNeto = salarioBruto - salud - pension - embargo;
+
+        // Mostrar resultados
+        Console.WriteLine("\nInformación del empleado:");
+        Console.WriteLine("Nombre: " + nombreEmpleado);
+        Console.WriteLine("Cargo: " + cargoEmpleado);
+        Console.WriteLine("Salario Neto (antes de deducciones): $" + salarioBruto.ToString("3.90"));
+        Console.WriteLine("Salario Final (después de deducciones): $" + salarioNeto.ToString("3.90."));
+
+        Console.ReadLine(); // Esperar a que el usuario presione Enter
+    }
+}
