5d3b4a0d798ac376644e390fa31a7da0afc956fd
carlosmena2191 <carloselrussomena5@gmail.com>
2023-09-01 21:18:27 -0500

    Punto1

--- Punto1/Program.cs
+++ Punto1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Realice un programa en C# donde se tenga la posibilidad de evaluar la siguiente expresión matemáticay me permita encontrar el valor de la variable "𝒙":
+
+using System;
 
 namespace Punto1
 {
@@ -14,6 +16,13 @@ namespace Punto1
             int a = random.Next(min, max);
             int b = random.Next(min, max);
             int c = random.Next(min, max);
+
+            // calcular el valor de x utilisando la expresion matematica
+            double x = Math.Sqrt(Math.Pow(3 * a + 4 * b, 3) / Math.Pow(2 * c + a, 2));
+
+            //imprimir el valos de x
+            Console.WriteLine("el valor de x es:")
+
         }
     }
 }
