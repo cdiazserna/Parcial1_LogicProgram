06032483dd1528869b020c8e5bd5609ae6df53e0
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 21:55:15 -0500

    Punto 2 Finalizado

--- Punto2/Punto2/Program.cs
+++ Punto2/Punto2/Program.cs
@@ -8,7 +8,7 @@
         Console.Write("Ingrese el nombre del empleado: ");
         nombreEmpleado = Console.ReadLine();
         Console.Write("Ingrese el cargo del empleado: ");
-        String cargo = cargoEmpleado = Console.ReadLine();
+        cargoEmpleado = Console.ReadLine();
         Console.Write("Ingrese las horas trabajadas en el mes:");
         horasTrabajadas = Convert.ToDouble(Console.ReadLine());
         Console.Write("Ingrese el valor por hora trabajada: ");
@@ -18,7 +18,7 @@
 
         salarioFinal = (salarioNeto - ((((4 * salarioNeto) / 100) + (4 * salarioNeto) / 100) + (salarioNeto/4)));
 
-        Console.Write()
+        Console.Write("El salario final es: " + salarioFinal);
     }
 
 
