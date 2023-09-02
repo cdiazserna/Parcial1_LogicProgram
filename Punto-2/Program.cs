d49272a48d8ba1b9c61a6ad582e6eefb0f2926e3
omora79 <oamm30@hotmial.com>
2023-09-01 21:08:53 -0500

    Declaracion de variables

--- Punto-2/Program.cs
+++ Punto-2/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
 namespace Punto_2
+
+    //: Desarrolle un algoritmo en C# que me permita calcular la nómina del mes de un empleado.
+    //Debemos pedir por pantalla el nombre del empleado, el cargo, horas trabajadas y valor de la hora
+    //Se debe descontar de su salario el 4% correspondientes a salud, el 4% correspondientes a pensión y
+    //¼ parte por un embargo bancarios que recibió dicho empleado
+    //Mostrar por pantalla el nombre del empleado, su cargo, salario neto (antes de deducciones) 
+    // y salario final (después de deducciones).
+
 {
-    class Program
+    // 1 clase y metodo
+    public class Punto2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 2 declaracion de varibles
+
+            string Nombre, cargo;
+            double nomina, horas, valorHora, embargo, nominaTotal;
         }
     }
 }
