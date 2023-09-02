70f1b996bed9973b5fad6845a4fb6db29ff2ebbd
soxxa6@gmail.com <soxxa6@gmail.com>
2023-09-01 21:40:50 -0500

    Inicio

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Parcial1
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int min = 1;  // Límite inferior del rango
+        int max = 10; // Límite superior del rango (max - min = 9, que es menor que 8)
+
+        int a = random.Next(min, max + 1);
+        int b = random.Next(min, max + 1);
+        int c = random.Next(min, max + 1);
+
+        double x = Math.Sqrt(Math.Pow(3 * a + 4 * b, 3) / (2 * c * Math.Pow(2 * c + a, 2)));
+
+        Console.WriteLine($"a: {a}");
+        Console.WriteLine($"b: {b}");
+        Console.WriteLine($"c: {c}");
+        Console.WriteLine($"x: {x}");
+    }
+}
