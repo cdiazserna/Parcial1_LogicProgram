0fa41f5c7472ce72a538c1264c46f483f728a878
DubierArismendy <dubiermontoya1116894@correo.itm.edu.co>
2023-09-01 20:47:55 -0500

    Guardar avance

---
+++ Punto1/Punto1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Realice un programa en C# donde se tenga la posibilidad de evaluar la siguiente expresión matemática y me permita encontrar el valor e
+//decimal la variable x
+
+namespace Punto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declarar variables
+            double a, b, c, x;
+
+            //Asignación de números aleatorios
+
+            Random random = new Random();
+
+            a = random.Next(2,10);
+            b = random.Next(2,10);
+            c = random.Next(2,10);
+
+            //
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
