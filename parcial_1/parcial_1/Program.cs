08c3bbb89439dec4cd85de275ddbf0576cf715cf
Julian1824 <julianbs1813@gmail.com>
2023-09-01 19:03:58 -0600

    Inicio del punto 1

--- parcial_1/parcial_1/Program.cs
+++ parcial_1/parcial_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ensayo");
+﻿/*
+ Un vendedor recibe un sueldo base más un 10% extra por comisión de sus ventas, 
+el vendedor desea saber cuánto dinero obtendrá por concepto de comisiones por las tres 
+ventas que realiza en el mes y el total que recibirá en el mes tomando en cuenta 
+su sueldo base y comisiones.
+ */
+class Program
+{
+
+    static void Main()
+    {
+     
+    }
+}
