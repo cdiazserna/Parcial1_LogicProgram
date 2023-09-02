de48f6bc12eafae82e88108f9882303dd8f0fcaa
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-01 20:28:59 -0500

    ingresar los valores

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// realizar punto 1
+
+public class Punto1
+{
+    private static int randomNumber;
+
+    static void Main(string[] args )
+    {
+        Random random = new Random();
+        randomNumber = random.Next();
+
+        double a, b, c;
+        Console.Write("ingrese el valor de a:");
+       
+
+    }
+}
