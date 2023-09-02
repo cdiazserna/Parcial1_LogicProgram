d5b8e90836225cc4b001db160dab4ec260766d4c
Marlon Foronda <forondamarlon642@gmail.com>
2023-09-01 21:27:34 -0500

    Solución punto 1 parcial 1

--- Punto1/Program.cs
+++ Punto1/Program.cs
@@ -5,19 +5,24 @@ public class Punto1Parcial
     static void Main(string[] args)
        
     {   //2 Declaracion de mis variables
-        int x, randomNumber, a, b, c;
-
-
-        //3 Pedir los valores para la consola
-        
+        int x, randomNumber, a, b, c, variablex;
+      
         Random random = new Random();
         randomNumber = random.Next(1, 8);
+        //3 Pedir los valores para la consola
+        num1 = 3
+        num2 = 4
+        num3 = 2
+
 
         //4to Cálculos (procesos)
 
-        x = math.sqrt(math.pow(3 * a + 4 * b, 3)math.pow(2) * c / math.pow(2 * c + a, 2);
+        variablex = math.sqrt(math.pow(num1 * a + num2 * b, 3))(math.pow(2)) * c / math.pow(num3 * c + a, 2);
 
-        
+        //5to imprimir los resultados (Datos de salida)
+        console.write("Muestre el valor de x: " + variablex );
+
+
 
 
 
