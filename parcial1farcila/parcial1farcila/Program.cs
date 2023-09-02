5bc5d9b5fa40f26f13b6e42388e3eeb36a7d4f06
farcila13 <frankliin.arcila@gmail.com>
2023-09-01 21:56:20 -0500

    Calcula (procesos)

--- parcial1farcila/parcial1farcila/Program.cs
+++ parcial1farcila/parcial1farcila/Program.cs
@@ -12,9 +12,18 @@ namespace parcial1farcila
             {
                 // Generacion de la instancia
                 Random random = new Random();
-            A = random.Next(4, 12);
-            B = random.Next(4, 12);
-            C=random.Next (4,12)
+                A = random.Next(4, 12);
+                B = random.Next(4, 12);
+                C = random.Next(4, 12);
+                //Calculos (Proceso)
+                double X1 = Math.Pow(3, (3 * A + 4 * B));
+                double X2 = Math.Pow(2, X1 * C);
+                double X3 = Math.Pow(2, 2 * C + A);
+                double X = Math.Sqrt(X2 / X3);
+                Console.Write($"El valor de X es:{X}");
+
+
+            }
         }
     }
 }
