414d00b19f9cda5f2ac186c5de45bcd67998565b
Marlon Foronda <forondamarlon642@gmail.com>
2023-09-01 21:55:50 -0500

    solución punto 2 parcial 1

--- Punto 2/ConsoleApp1/Program.cs
+++ Punto 2/ConsoleApp1/Program.cs
@@ -3,14 +3,20 @@ public class MiPrimerPuntoTaller
 {
     static void Main(string[] args)
     {   //declaración de mis variables
-        int nomina, salario, salud, pensión, banco;
+        double salarioneto, horastrabajo, valorhoras, salariobase;
+        double pension= 4
+    
         //pedir valores de consola 
         Console.Write("Ingrese el nombre del empleado");
         Console.Write("Ingrese el cargo del empleado");
         console.write("Ingrese el salario neto del empleado");
-        console.write("Ingrese el nombre del empleado");
-
+        salarioneto = Convert.ToDouble(Console.ReadLine());
+        console.write("Ingrese horas trabajadas");
+        horastrabajo = Convert.ToDouble(Console.ReadLine());
+        console.write("Ingrese valor de las horas");
+        valorhoras = Convert.ToDouble(Console.ReadLine());
         //4to Calculos (procesos)
+        salarioneto = salariobase - salud-pensión-banco
 
 
 
