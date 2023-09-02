fb0157efb6f5a31928c177bbcaec7aa2c147a17d
Daniel Amado Garcia <danielamadogarcia8@gmail.com>
2023-09-01 21:38:38 -0500

    definicion de variables del 4%

--- Punto2/Program.cs
+++ Punto2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar información del empleado
+        Console.WriteLine("Calculadora de Nómina Mensual");
+        Console.Write("Nombre del empleado: ");
+        string nombreEmpleado = Console.ReadLine();
+        Console.Write("Cargo del empleado: ");
+        string cargoEmpleado = Console.ReadLine();
+        Console.Write("Horas trabajadas: ");
+        double horasTrabajadas = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Valor de la hora: ");
+        double valorHora = Convert.ToDouble(Console.ReadLine());
+
