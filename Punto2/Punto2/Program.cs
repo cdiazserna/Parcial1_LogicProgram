0a080cbcd7fa544ead991677b9dd7dfc6815e06b
santi04 <santivelas5@gmail.com>
2023-09-01 20:28:58 -0500

    Proceso deducciones de nomina

--- Punto2/Punto2/Program.cs
+++ Punto2/Punto2/Program.cs
@@ -3,8 +3,23 @@
     static void Main(string[] args)
     {
         //declaración variables
+        string empleado, cargo;
+        double horasTrabajadas;
+        decimal valorHora, pagoCompleto, pagoDeducciones;
 
         //datos de entrada
+        Console.WriteLine("Ingrese el nombre del empleado: ");
+        empleado = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el cargo del empleado: ");
+        cargo = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la cantidad de horas trabajadas: ");
+        horasTrabajadas = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de la hora: ");
+        valorHora = decimal.Parse(Console.ReadLine());
+
 
         //proceso
 
