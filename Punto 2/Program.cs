a7cdf15a5b11a10bfddfce8e521e3350da8aea5c
SantiagoFU - BK <santiagoflorez287345@correo.itm.edu.co>
2023-09-01 21:50:03 -0500

    Error mio, inicio el programa, declaro variables, randomizo y opero, finalizo, cambio nombre class. Inicio punto 2, declaro variables, solicito información, se agregan variables necesarias y se empieza a operar, se termina de corregir variables y operar, se termina operación y se empieza print, se empieza prueba, se agrega decimal, se finaliza punto 2

---
+++ Punto 2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class Punto2
+{
+    static void Main(string[] args)
+    //Peso: 50%Punto 2: Desarrolle un algoritmo en C# que me permita calcular la nómina del mes de un empleado. Debemos pedir por pantalla el nombre del empleado,
+    //el cargo, horas trabajadas y valor de la hora.Se debe descontar de su salario el 4% correspondientes a salud,
+    //el 4% correspondientes a pensión y ¼ parte por un embargo bancarios que recibió dicho empleadoMostrar por pantalla el nombre del empleado, su cargo,
+    //salario neto (antes de deducciones) y salario final (después de deducciones).
+    {
+        string nombreEmpleado, cargo;
+        double horasTrabajadas, valorHoras, salarioNeto, descuentoSaludPension, descuentoEmbargo, salarioAPagar;
+
+        
+        Console.WriteLine("Ingrese su nombre: ");
+        nombreEmpleado = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su cargo: ");
+        cargo = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la cantidad de horas trabajadas: ");
+        horasTrabajadas = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de las horas: ");
+        valorHoras = double.Parse(Console.ReadLine());
+
+        salarioNeto = decimal.(valorHoras * horasTrabajadas);
+        descuentoSaludPension = decimal(salarioNeto * 0.08);
+        descuentoEmbargo = decimal.(salarioNeto / 4);
+        salarioAPagar = decimal.(salarioNeto - (descuentoSaludPension + descuentoEmbargo));
+
+        Console.WriteLine("Empleado " + nombreEmpleado + " - " + cargo + "su salario neto es $" + salarioNeto + "y su salario después de deducciones es $" + salarioAPagar);
+
+    }
+}
+
+
---
+++ Punto1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class Punto1
+{
+    static void Main(string[] args)
+
+
+    {
+        Random random = new Random();
+
+        double randomNumberA, randomNumberB, randomNumberC, a, b, c, x;
+        randomNumberA = random.Next(1, 8);
+        randomNumberB = random.Next(2, 8);
+        randomNumberC = random.Next(3, 8);
+
+        a = randomNumberA;
+        b = randomNumberB;
+        c = randomNumberC;
+
+        x = (Math.Sqrt(((Math.Pow(Math.Pow((3 * a) + (4 * b), 3), 2) * c) / (Math.Pow(((2 * c) + a), 2)))));
+
+        Console.WriteLine("El valor de la variable x es: " + x);
+
+    }
+}
