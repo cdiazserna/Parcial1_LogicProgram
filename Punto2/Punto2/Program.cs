921c24f61fc273c6da69ee3b4546e2fac38f628d
SANTIAGOGALVIS04 <sg1904@gmail.com>
2023-09-01 21:37:22 -0500

    Declaramos el tipo de variable para porder calcular el salario y pedir informacion

---
+++ Punto2/Punto2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Punto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Desarrolle un algoritmo de C # que me permita calcular la nómina del mes de un empleado. Debemios
+            //pedir por pantalla el nombre del empleado, el cargo, horas trabajadas y el valor de la hora
+
+            //Declaramos los tipos de variables
+
+            String nombreEmpleado, cargoEmpleado;
+            double horasTrabajadas, valorHora;
+
+
+
+        }
+    }
+}
