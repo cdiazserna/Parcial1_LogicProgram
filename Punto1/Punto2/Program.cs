e64f3968e1eb5d980fed61c4292814e4270d239a
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-01 21:49:05 -0500

    Definicion de variables

---
+++ Punto1/Punto2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Punto 2
+public class Punto2
+{
+    static void Main(string[] agrs)
+    {
+        double nombreEmpleado, cargo, horasTrabajadas, valorHora;
+
+        Console.Write("ingrese el el nombre del empleado");
+    }
+
+}
+    
