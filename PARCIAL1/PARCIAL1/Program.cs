fa562960ec90503d90c58aa20ae7699c33f7078f
jenihernandez <jenihernandez.98@gmail.com>
2023-09-01 20:56:39 -0500

    definir variables

---
+++ PARCIAL1/PARCIAL1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PARCIAL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese nombre del empleado: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Cargo del empleado: ");
+            string cargo = Console.ReadLine();
+
+            Console.WriteLine("Horas trabajadas por el empleado: ");
+            int horasTrabajadas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor de la hora: ");
+            double valorHora = double.Parse(Console.ReadLine());
+
+            double salarioNeto 
+
+         
+        }
+    }
+}
