53a9b16057b35378a4c7a581002ddf34d2b469d8
Julian1824 <julianbs1813@gmail.com>
2023-09-01 19:19:28 -0600

    Se realiza la primera prueba

--- Punto 1/Punto 1/Program.cs
+++ Punto 1/Punto 1/Program.cs
@@ -5,12 +5,21 @@ class Punto_1
 
     static void Main()
     {
-        double x, a, b, c,randomNumber;
+        double x, a, b, c;
 
         Random rand = new Random();
 
-        randomNumber = rand.Next(10,18);
+        a = rand.Next(10,18);
+        b = rand.Next(10, 18);
+        c = rand.Next(10, 18);
 
-        Console.WriteLine(randomNumber);
+        x = Math.Sqrt(Math.Pow(Math.Pow((3 * a + 4 * b), 3),2)*c/Math.Pow((2*c+a),2));
+
+        Console.WriteLine($"Valor de a: {a}");
+        Console.WriteLine($"Valor de b: {b}");
+        Console.WriteLine($"Valor de c: {c}");
+        Console.WriteLine(x);
+
+
     }
 }
