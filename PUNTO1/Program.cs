6fe2a6c94adbcb8e84658d1ce7325984b70c230a
JUAN PABLO UPEGUI ORTIZ <upegui2007@hotmail.com>
2023-09-01 21:16:27 -0500

    CALCULAR VALORES

--- PUNTO1/Program.cs
+++ PUNTO1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Punto 1: Realice un programa en C# donde se tenga la posibilidad de evaluar la siguiente expresión 
+//matemática y me permita encontrar el valor de la variable "𝒙":
+
+public class PUNTO1
+{
+
+    static void MAIN(string[] args)
+    {
+        //DATOS DE ENTRADA
+        int A, B, C, Radom,Randomnumber, Min,Max, VALOR1, VALOR2, VALOR3, X;
+        Random random = new Random();
+        Randomnumber = Radom.Next(Min, Max);
+        Min < 8;
+        Max > 9999999999);
+             
+
+        Console.WriteLine("INGRESA EL VALOR DE A");
+        A = int Randomnumber(Console.ReadLine())
+     ;
+        Console.WriteLine("INGRESA EL VALOR DE B");
+        B = Randomnumber (Console.ReadLine())
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+Console.WriteLine("");
