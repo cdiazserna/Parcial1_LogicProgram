d95e360d0a68bea74512f7812fa46e3cdc803d7b
DubierArismendy <dubiermontoya1116894@correo.itm.edu.co>
2023-09-01 21:01:39 -0500

    Descripción

---
+++ Punto2/Punto2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Desarrolle un algoritmo en C# que me permita calcular la nómina del mes de un empleado.
+//Debemos pedir por pantalla el nombre del empleado, el cargo, horas trabajadas y el valor de la hora.
+
+//Se debe descontar de su salario el 4% correspondientes a salud, el 4% correspondientes a pensión y 1/4 parte por un
+//embargo bancario que recibió dicho empleado
+
+//Mostrar por pantalla el nombre del empleado, su cargo, salario neto (antes de deducciones) y salario final (despues de deducciones).
+namespace Punto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
