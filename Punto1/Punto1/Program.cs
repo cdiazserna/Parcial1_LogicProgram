9599a4bc72b1f87e88f75353bdb596e2ad4e6262
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 20:54:46 -0500

    Intento 1

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -1,1 +1,31 @@
-﻿// 
+﻿public class Punto1
+{
+   
+    static void Main(string[] args, object valorA)
+    {
+        double Xx, valorA, Bb, Cc;
+
+
+        Random random = new Random();
+        valorA = random.Next(1,9);
+        valorA = Convert.ToDouble(valorA);
+
+        
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
