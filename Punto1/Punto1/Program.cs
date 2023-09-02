c9ef967b8a69367b13c259a4ca7007e869d456e9
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 21:24:54 -0500

    Proyecto finalizado

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -5,21 +5,13 @@
         double X, A, B, C;
 
 
-
         Random random1 = new Random();
         A = random1.Next(1, 9);
-       
-
         Random random2 = new Random();
         B = random2.Next(1, 9);
-        
-
         Random random3 = new Random(); 
         C = random3.Next(1, 9);
-        
-
         X = Math.Sqrt((Math.Pow(Math.Pow(3 * A + 4 * B, 3), 2)*C) / Math.Pow(2*C + A, 2));
-
         Console.Write("El valor de x es: " + X);
 
  
