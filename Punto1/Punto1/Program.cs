daaca331a9aceb78b106792253c82ea77a138179
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 21:24:07 -0500

    Finalizacion formula

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -1,23 +1,28 @@
 ﻿public class Punto1
 {
-   
-    static void Main(string[] args, object valorA)
+    static void Main(string[] args)
     {
-        double Xx, valorA, Bb, Cc;
+        double X, A, B, C;
 
 
-        Random random = new Random();
-        valorA = random.Next(1,9);
-        valorA = Convert.ToDouble(valorA);
 
+        Random random1 = new Random();
+        A = random1.Next(1, 9);
+       
+
+        Random random2 = new Random();
+        B = random2.Next(1, 9);
         
 
+        Random random3 = new Random(); 
+        C = random3.Next(1, 9);
+        
 
+        X = Math.Sqrt((Math.Pow(Math.Pow(3 * A + 4 * B, 3), 2)*C) / Math.Pow(2*C + A, 2));
 
+        Console.Write("El valor de x es: " + X);
 
-
-
-
+ 
 
     }
 
