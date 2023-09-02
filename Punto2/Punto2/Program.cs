0fca766670c362cc048d69cfe0ff33772ae71e6b
DubierArismendy <dubiermontoya1116894@correo.itm.edu.co>
2023-09-01 21:07:16 -0500

    GUARDAR

--- Punto2/Punto2/Program.cs
+++ Punto2/Punto2/Program.cs
@@ -6,13 +6,29 @@
 //Se debe descontar de su salario el 4% correspondientes a salud, el 4% correspondientes a pensión y 1/4 parte por un
 //embargo bancario que recibió dicho empleado
 
-//Mostrar por pantalla el nombre del empleado, su cargo, salario neto (antes de deducciones) y salario final (despues de deducciones).
+//Mostrar por pantalla el nombre del empleado, su cargo, salario neto (antes de deducciones)
+//y salario final (despues de deducciones).
+
+
 namespace Punto2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Declarar variables
+
+            string nombreEmpleado, cargo;
+            double horasTrabajadas, valorHora;
+
+            //Asignar valor a las variables o pedir data
+
+            Console.WriteLine("Ingrese el nombre del empleado: ");
+            nombreEmpleado = String.Format(Console.ReadLine());
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
