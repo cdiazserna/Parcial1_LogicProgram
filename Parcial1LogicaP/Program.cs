ea882a6cb3975e13c931e02eb74ffef090dc654f
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2023-09-01 21:16:21 -0500

    Primera parte del parcial

---
+++ Parcial1LogicaP/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+public class parcial1Logicap
+{
+    static void Main(string[] args)
+    {
+        double a, b, c, x;
+
+
+        Random randon = new Random();
+        
+        a = randon.Next(1,10);
+        b = randon.Next(1,10);
+        c = randon.Next(1,10);
+
+        x = Math.Sqrt(Math.Pow(Math.Pow(3 * a + 4 * b, 3), 2) * c / Math.Pow(2 * c + a, 2));
+         
+        Console.Write("x es igual a: " + x);
+        Console.Write("\n");
+    }
+
+
+
+
+
+
+
+
+
+}
