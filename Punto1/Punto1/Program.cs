720436fa6372cd731791d0b6c7e528d3144efc97
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-01 20:44:01 -0500

    calcular el valor de x

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -4,14 +4,26 @@ public class Punto1
 {
     private static int randomNumber;
 
-    static void Main(string[] args )
+    static void Main(string[] args)
     {
         Random random = new Random();
         randomNumber = random.Next();
 
         double a, b, c;
+
         Console.Write("ingrese el valor de a:");
-       
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el valor de b:");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el valor de c:");
+        c = Convert.ToDouble(Console.ReadLine());
+
+      
+
+        //calcular el valor de x
+        
 
     }
 }
