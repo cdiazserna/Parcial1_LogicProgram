2ae58dda2dd70c296a04443e77306c74f63dd835
Marlon Foronda <forondamarlon642@gmail.com>
2023-09-01 20:53:33 -0500

    Solución Punto 1 parcial 1

---
+++ Punto1/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1 Creo mi clase y su respectivo metodo main 
+public class Punto1Parcial
+
+{
+    static void Main(string[] args)
+    {//2 Declaracion de mis variables
+        int x, randomNumber, a, b, c;
+
+    }
+      
+
+        
+       
+
+        //3 Pedir los valores para la consola
+
+        
+
+   
+}
