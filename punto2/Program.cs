12c4de0baea2b8052458e422b87ede6d99e7bb0a
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-01 21:40:47 -0500

    plantilla

--- punto2/Program.cs
+++ punto2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//Desarrolle un algoritmo en C# que me permita calcular la nómina del mes de un empleado. 
+//Debemos pedir por pantalla el nombre del empleado, el cargo, horas trabajadas y valor de la hora
+
+namespace punto1
+{
+    public class Punto1
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
