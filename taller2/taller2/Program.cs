83f4206c07f5509b93c07759650958724f2d71e5
luismiguelh <luismiguelhh01@hotmail.com>
2023-09-01 21:15:36 -0500

    guardar taller 2

---
+++ taller2/taller2/Program.cs
@@ -0,0 +1,30 @@
+﻿// SeeDesarrolle un algoritmo en C# que me permita calcular la nómina del mes de un empleado. Debemos pedir
+// pantalla el nombre del empleado, el cargo, horas trabajadas y valor de la hora.
+
+
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+class taller2
+
+{
+    static void Main(string[] args)
+
+    {
+        string nombreLacallo, cargo;
+        double horaTrabajo, valorHoras;
+
+        Console.WriteLine("Igrese nombre del usuario");
+        nombreLacallo = Console.ReadLine();
+
+        Console.WriteLine("Ingrese cargo");
+        cargo = Console.ReadLine(); 
+
+        Console.WriteLine("Ingrese horas");
+        horaTrabajo = double.Parse(cargo);
+
+        Console.WriteLine("Ingrese valor");
+        valorHoras
+
+    }
+}
