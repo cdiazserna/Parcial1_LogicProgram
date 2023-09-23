72131b63e850189541f82e1f2d8435f0cdba86a6
luismiguelh <luismiguelhh01@hotmail.com>
2023-09-22 21:05:15 -0500

    commit dos

--- taller2/clas20-09/parcial2/Program.cs
+++ taller2/clas20-09/parcial2/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 
 namespace parcial2
 {
     static void Main(string[] args)
     {
+        Random rnd = new Random();
         //D. Variable
         int rangoMinimo = 1;
         int rangoMaximo = 100;
@@ -19,5 +21,48 @@ namespace parcial2
         {
            Console.Write("Introduce tu suposición: ");
                 string entrada = Console.ReadLine();
+            if (int.TryParse(entrada, out int suposicion))
+            {
+                intentos++;
+
+                if (suposicion == numeroAleatorio)
+                {
+                 Console.WriteLine($"¡Felicitaciones! Adivinaste el número en {intentos} intentos.");
+                   true adivinado = adivinado;
+                 
+                         }
+                else if (suposicion < numeroAleatorio)
+                {
+                 Console.WriteLine("El número que estoy pensando es mayor. ¡Sigue intentando!");
+                
+                   }
+                else
+                  {
+                 Console.WriteLine("El número que estoy pensando es menor. ¡Sigue intentando!");
+                                                
+                else
+                {
+                 Console.WriteLine("Por favor, ingresa un número válido.");
+                 Console.WriteLine("¡Gracias por jugar!");
+        }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
 
                
