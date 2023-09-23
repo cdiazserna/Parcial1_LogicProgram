5c4f463e5417c672d346f58fc675e8f9899562bc
luismiguelh <luismiguelhh01@hotmail.com>
2023-09-22 20:54:17 -0500

    guardar commis

--- taller2/clas20-09/parcial2/Program.cs
+++ taller2/clas20-09/parcial2/Program.cs
@@ -1,26 +1,23 @@
-﻿namespace parcial2
+﻿using System;
+
+namespace parcial2
 {
     static void Main(string[] args)
     {
         //D. Variable
-        int numero, numeroJugador;
-        
-        Random numero = new Random();
+        int rangoMinimo = 1;
+        int rangoMaximo = 100;
+        int numeroAleatorio = Random.Next(rangoMinimo, rangoMaximo + 1);
+        int intentos = 0;
+        bool adivinado = false;
 
-        Console.WriteLine("adivina y gana\n");
+        Console.WriteLine("¡Bienvenido al juego 'Adivina el número'!");
 
-        Console.WriteLine("digite el numero aleatorio");
+        Console.WriteLine($"Estoy pensando en un número entre {rangoMinimo} y {rangoMaximo}. ¿Puedes adivinar cuál es?");
 
+        while (adivinado)
+        {
+           Console.Write("Introduce tu suposición: ");
+                string entrada = Console.ReadLine();
 
-        
-    
-    
-    
-    
-    
-    
-    }
-
-
-
-}
+               
