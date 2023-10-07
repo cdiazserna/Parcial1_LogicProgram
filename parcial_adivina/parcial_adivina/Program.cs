db847a6f35ded8611f3673674fd9a061162a55fe
Julian1824 <julianbs1813@gmail.com>
2023-10-07 18:53:29 -0500

    Commit

---
+++ parcial_adivina/parcial_adivina/Program.cs
@@ -0,0 +1,154 @@
+﻿class Program
+{
+   static void Main()
+    {
+        int cantJugadores = 0,numeroGanador,numeroEscoger;
+        String continuarJugando;
+        bool jugarVariable = true, partidaIniciada = true;
+        Random aleatorio = new Random();
+
+        
+        do
+        {
+
+            Console.WriteLine("Cantidad de integrantes para jugar es de minimo 2 maximo 4");
+            cantJugadores = Convert.ToInt32(Console.ReadLine());
+
+            if (cantJugadores >= 2 && cantJugadores <= 4)
+            {
+
+                if (cantJugadores == 2) {
+                    int jugadores = 2;
+                    numeroGanador = aleatorio.Next(0, 50);
+                    while (partidaIniciada) {
+
+                        Console.WriteLine("Numero ganador" + numeroGanador);
+
+                        if (jugadores == 2)
+                        {
+                            jugadores = 1;
+                        }
+                        else if (jugadores == 1) {
+                            jugadores = 2;
+                        }
+
+
+                        Console.WriteLine($"Por favor dijite un numero jugador {jugadores}");
+                        numeroEscoger = Convert.ToInt32(Console.ReadLine());
+                        if (numeroEscoger == numeroGanador) {
+                            Console.WriteLine($"¡HAS GANADO! jugador {jugadores}");
+                            partidaIniciada = false;
+                        } else if (numeroEscoger > numeroGanador) {
+                            Console.WriteLine("Menor");
+                        }
+                        else if (numeroEscoger < numeroGanador)
+                        {
+                            Console.WriteLine("Mayor");
+                        }
+                    }
+
+                }
+                else if (cantJugadores == 3)
+                {
+                    int jugadores = 3;
+                    numeroGanador = aleatorio.Next(0, 100);
+                    while (partidaIniciada)
+                    {
+
+                        Console.WriteLine("Numero ganador" + numeroGanador);
+
+                        if (jugadores == 3)
+                        {
+                            jugadores = 1;
+                        }
+                        else if (jugadores == 1)
+                        {
+                            jugadores = 2;
+                        }
+                        else if (jugadores == 2)
+                        {
+                            jugadores = 3;
+                        }
+
+
+                        Console.WriteLine($"Por favor dijite un numero jugador {jugadores}");
+                        numeroEscoger = Convert.ToInt32(Console.ReadLine());
+                        if (numeroEscoger == numeroGanador)
+                        {
+                            Console.WriteLine($"¡HAS GANADO! jugador {jugadores}");
+                            partidaIniciada = false;
+                        }
+                        else if (numeroEscoger > numeroGanador)
+                        {
+                            Console.WriteLine("Menor");
+                        }
+                        else if (numeroEscoger < numeroGanador)
+                        {
+                            Console.WriteLine("Mayor");
+                        }
+                    }
+                }
+                else if (cantJugadores == 4)
+                {
+                    int jugadores = 4;
+                    numeroGanador = aleatorio.Next(0, 200);
+                    while (partidaIniciada)
+                    {
+
+                        Console.WriteLine("Numero ganador" + numeroGanador);
+
+                        if (jugadores == 4)
+                        {
+                            jugadores = 1;
+                        }
+                        else if (jugadores == 1)
+                        {
+                            jugadores = 2;
+                        }
+                        else if (jugadores == 2)
+                        {
+                            jugadores = 3;
+                        }
+                        else if (jugadores == 3)
+                        {
+                            jugadores = 4;
+                        }
+
+
+                        Console.WriteLine($"Por favor dijite un numero jugador {jugadores}");
+                        numeroEscoger = Convert.ToInt32(Console.ReadLine());
+                        if (numeroEscoger == numeroGanador)
+                        {
+                            Console.WriteLine($"¡HAS GANADO! jugador {jugadores}");
+                            partidaIniciada = false;
+                        }
+                        else if (numeroEscoger > numeroGanador)
+                        {
+                            Console.WriteLine("Menor");
+                        }
+                        else if (numeroEscoger < numeroGanador)
+                        {
+                            Console.WriteLine("Mayor");
+                        }
+                    }
+                }
+            }
+            else {
+                Console.WriteLine("El número de participantes que jugarán debe ser de :" +
+                    " mínimo 2 y máximo 4 integrantes.");
+                jugarVariable = true;
+            }
+
+
+            Console.WriteLine("Desea seguir Jugando s/n");
+            continuarJugando = Console.ReadLine().ToLower();
+
+            if (continuarJugando == "n")
+            {
+                jugarVariable = false;
+            }
+
+
+        } while (jugarVariable);
+    }
+}
---
+++ switch_case/switch_case/Program.cs
@@ -0,0 +1,82 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+        /* Realice un programa que solicite dos
+         * números, luego debe pedirse un número que seleccionará
+         * alguna operación aritmética desde un menú. Si introduce 1 los números
+         * se deben sumar, si introduce 2 los números se deben restar, si es 3 los números
+         * se multiplican y si introduce 4 los números se dividen (validar división por cero).
+         * Independientemente de la selección, imprima el resultado. Además, el programa debe
+         * tener la posibilidad de volver al menú para pedir 2 
+         * nuevos números y hacer la elección de la operación aritmética o digitar cero para
+         * finalizar el programa.  */
+
+
+        double num1, num2,operacion;
+        bool repetir = false;
+        int seleccion;
+        String continuar;
+
+
+
+        do
+        {
+            Console.WriteLine("Bienvenido a tu calculadora de confianza");
+
+
+
+            Console.WriteLine("Por favor, selecione una opción\n *1 Sumar \n *2 Restar \n *3 Multiplicar \n *4 División");
+            seleccion = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Por favor, dijitar el primer numero");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Por favor, dijitar el segundo numero");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (seleccion) {
+
+                case 1:
+                    operacion = num1 + num2;
+                    Console.WriteLine(operacion);
+                    break;
+
+                case 2:
+
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                  
+                   break;
+
+                default:
+                    Console.WriteLine("Por valide su sorpresa");
+                    break;
+
+
+            }
+
+            
+
+            Console.WriteLine("Desea calcular nuevamente s/n");
+            continuar = Console.ReadLine().ToLower();
+
+            if (continuar == "n")
+            {
+                repetir = false;
+            }
+            else {
+                repetir = true;
+            }
+
+        } while (repetir);
+    }
+}
